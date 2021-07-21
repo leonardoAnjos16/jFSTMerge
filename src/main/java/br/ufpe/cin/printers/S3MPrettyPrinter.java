7473df18cf9e8341ee41e97a45d306dff67e8c38
Leonardo Anjos <las4@cin.ufpe.br>
2021-07-21 17:48:32 -0300

    Fixed some linting problems

--- src/main/java/br/ufpe/cin/printers/S3MPrettyPrinter.java
+++ src/main/java/br/ufpe/cin/printers/S3MPrettyPrinter.java
@@ -11,20 +11,24 @@ import de.ovgu.cide.fstgen.ast.FSTTerminal;
 
 /**
  * Visitor to retrieve FSTNodes' contents to be printed.
- * 
  * @author João Victor (jvsfc@cin.ufpe.br)
  */
 public abstract class S3MPrettyPrinter extends AbstractFSTPrintVisitor {
 
     private final StringBuilder result;
     private final Queue<String> tokensCurrentLine;
-
-    private final Pattern conflictPattern = Pattern
-            .compile("<<<<<<< MINE(.*)(||||||| BASE)?(.*)=======(.*)>>>>>>> YOURS(.*)", Pattern.DOTALL);
+    private final Pattern conflictPattern;
 
     public S3MPrettyPrinter() {
         this.result = new StringBuilder();
         this.tokensCurrentLine = new LinkedList<String>();
+
+        String conflictRegex = "<<<<<<< MINE(.*)";
+        conflictRegex += "(||||||| BASE)?(.*)";
+        conflictRegex += "=======(.*)";
+        conflictRegex += ">>>>>>> YOURS(.*)";
+        
+        this.conflictPattern = Pattern.compile(conflictRegex, Pattern.DOTALL);
     }
 
     private boolean printedStatementOnFirstLine = false;
@@ -36,11 +40,12 @@ public abstract class S3MPrettyPrinter extends AbstractFSTPrintVisitor {
 
         String token = "";
         if (body.isEmpty()) {
-            // Node has been removed, so the prefix is trimmed to avoid unnecessary blank lines
+            // Prefix is trimmed to avoid unnecessary blank lines
             token = IndentationUtils.removePostIndentationAndLineBreaks(prefix);
         } else if (hasConflict(body)) {
-            // Node content is inside conflict, so the prefix is trimmed and a new line is added
-            token = IndentationUtils.removePostIndentation(prefix) + "\n" + body;
+            // Prefix is trimmed and a new line is added
+            prefix = IndentationUtils.removePostIndentation(prefix);
+            token = prefix + "\n" + body;
         } else if (isFirstLineOfCode(prefix) && isImportOrPackageStatement(terminal)) {
             // Statements on the first line don't have line breaks before them
             if (printedStatementOnFirstLine) {
@@ -58,7 +63,13 @@ public abstract class S3MPrettyPrinter extends AbstractFSTPrintVisitor {
     }
 
     private boolean isImportOrPackageStatement(FSTTerminal terminal) {
-        return terminal.getType().equals("ImportDeclaration") || terminal.getType().equals("PackageDeclaration");
+        switch (terminal.getType()) {
+            case "ImportDeclaration":
+            case "PackageDeclaration":
+                return true;
+            default:
+                return false;
+        }
     }
 
     private boolean isFirstLineOfCode(String prefix) {
